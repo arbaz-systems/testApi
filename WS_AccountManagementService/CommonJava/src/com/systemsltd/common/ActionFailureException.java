c5851a0d4cc5bca919a5e31b54c93e37f0d8b3e4
arbaz-systems <111056040+arbaz-systems@users.noreply.github.com>
2023-05-10 19:43:50 +0500

    Delete WS_AccountManagementService directory

--- WS_AccountManagementService/CommonJava/src/com/systemsltd/common/ActionFailureException.java
+++
@@ -1,9 +0,0 @@
-package com.systemsltd.common;
-
-public class ActionFailureException extends RuntimeException {
-  private static final long serialVersionUID = 1L;
-  
-  public ActionFailureException(String message, Throwable throwable) {
-    super(message, throwable);
-  }
-}
